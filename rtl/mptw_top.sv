71502041df48dde47f21fe4cb656b21c77a8aff5
Gr4npasso <stefanomercogliano@gmail.com>
2025-05-27 11:16:11 +0200

    - Draft implementation (continued) for plb_lookup_stage - General refactoring - Pre-debug

--- rtl/mptw_top.sv
+++ rtl/mptw_top.sv
@@ -13,57 +13,126 @@ import mpt_pkg::*;
 `include "uninasoc_mem.svh"
 
 module mptw_top #(
+
+    localparam unsigned PLB_TRANSACTION_DATA_WIDTH  = 1;                        // Only interested in the hit response 
+    localparam unsigned PLB_TRANSACTION_ADDR_WIDTH  = $bits(plb_lookup_req_t); 
 ) (
-    // Control Port
-    input  logic clk_i,
-    input  logic rst_ni,
-    input  logic flush_i,                       // Flush signal to reset internal state
-    input  logic mptw_enable_i,                 // Enable the MPT (i.e. only for non M-mode code)
+    //////////////////
+    // Control Port //
+    //////////////////
 
-    // Transaction Port                         
-    input  spa_t_u spa_i,                       // Supervisor physical address input
-    input  mmpt_reg_t mmpt_reg_i,               // Memory Protection Table Register input
-    input  mpt_access_e access_type_i,          // Memory access type (read, write, execute)
-    input  logic transaction_valid_i,
-    output logic mptw_ready_o,                  // The MPT Walker is ready to serve a transaction
+    input  logic                clk_i,
+    input  logic                rst_ni,
+    input  logic                flush_i,                // Flush signal to reset internal state
+    input  logic                mptw_enable_i,          // Enable the MPT (i.e. only for non M-mode code)
 
-    // Error Port
-    output logic access_page_fault_o,          // Access fault output (indicates if access is not allowed) 
-    output page_format_fault_e format_error_o  // Format fault output (generic error in address translation)
+    //////////////////////
+    // Transaction Port //
+    //////////////////////
 
-    // Memory Port 
+    input  spa_t_u              spa_i,                  // Supervisor physical address input
+    input  mmpt_reg_t           mmpt_reg_i,             // Memory Protection Table Register input
+    input  mpt_access_e         access_type_i,          // Memory access type (read, write, execute)
+    input  logic                mptw_transaction_valid_i,
+    output logic                mptw_ready_o,           // The MPT Walker is ready to serve a transaction
+
+    ////////////////
+    // Error Port //
+    ////////////////
+
+    output logic                access_page_fault_o,    // Access fault output (indicates if access is not allowed) 
+    output page_format_fault_e  format_error_o,         // Format fault output (generic error in address translation)
+
+    //////////////////
+    // Memory Ports //
+    //////////////////
+
+    // PLB Cache Port
+    `DEFINE_MEM_MASTER_PORTS(plb_cache, PLB_TRANSACTION_DATA_WIDTH, PLB_TRANSACTION_ADDR_WIDTH)
+
+
     //`DEFINE_MEM_MASTER_PORTS(m),
 
     // Output Port
     //output plb_entry_t plb_entry_o,            // Output PLB entry (contains SDID, physical address, and permissions)
     //output logic allow_o                       // Access allowed output (indicates if access is allowed)
 );  
+
+    //////////////////////////////////////////////////////////////
+    //    _                 _                                   //
+    //   | |   ___  __ __ _| |_ __  __ _ _ _ __ _ _ __  ___     //
+    //   | |__/ _ \/ _/ _` | | '_ \/ _` | '_/ _` | '  \(_-<     //
+    //   |____\___/\__\__,_|_| .__/\__,_|_| \__,_|_|_|_/__/     //
+    //                       |_|                                //
+    //////////////////////////////////////////////////////////////
+
     ////////////////////////////
     // Localparam Declaration //
     ////////////////////////////
 
     localparam fetch_stage_datawidth        = $bits(mptw_transaction_t);
     localparam plb_lookup_stage_datawidth   = $bits(mptw_transaction_t);
+    localparam walking_stage_datawidth      = $bits(mptw_transaction_t);
+
+    //////////////////////////////////////
+    //    ___ _                _        //
+    //   / __(_)__ _ _ _  __ _| |___    //
+    //   \__ \ / _` | ' \/ _` | (_-<    //
+    //   |___/_\__, |_||_\__,_|_/__/    //
+    //         |___/                    //
+    //////////////////////////////////////
 
     /////////////////////////
     // Signals Declaration //
     /////////////////////////
 
+    mptw_transaction_t      input_transaction;
+    page_format_fault_e     fetch_exception_cause;
+
+    /////////////////////
+    // Bus Declaration //
+    /////////////////////
+
     `DECLARE_DATA_BUS( input_to_fetch       , fetch_stage_datawidth         );
     `DECLARE_DATA_BUS( fetch_to_pipe        , plb_lookup_stage_datawidth    );
     `DECLARE_DATA_BUS( pipe_to_plb_lookup   , plb_lookup_stage_datawidth    );
+    `DECLARE_DATA_BUS( plb_lookup_to_pipe   , walking_stage_datawidth       );
+    `DECLARE_DATA_BUS( pipe_to_walking      , walking_stage_datawidth       );
 
-    page_format_fault_e  fetch_exception_cause;
+    //////////////////////////////////////////////////////
+    //    ___    _      _      ___ _                    //
+    //   | __|__| |_ __| |_   / __| |_ __ _ __ _ ___    //
+    //   | _/ -_)  _/ _| ' \  \__ \  _/ _` / _` / -_)   //
+    //   |_|\___|\__\__|_||_| |___/\__\__,_\__, \___|   //
+    //                                     |___/        //
+    //////////////////////////////////////////////////////
 
-    /////////////////
-    // Fetch Stage //
-    /////////////////
+    // Here starts the MPTW frontend. The fronted selects a transaction, checks if it 
+    // is valid and then lookup into the PLB (if any). The goal of the frontend is to
+    // decide wether a transaction has to be walked or not.
 
-    // Input Handling logic
-    //      TBD
+    //////////////////////////
+    // Input Handling logic //
+    //////////////////////////
 
-    // Fetch Stage 
-    fetch_stage # (
+    // Build the transaction out of the input signals
+    assign input_transaction.mmpt           = mmpt_reg_i;
+    assign input_transaction.spa            = spa_i;
+    assign input_transaction.access_type    = access_type_i;
+    assign input_transaction.walking        = MPT_WALKING_DO;
+
+    // Build `input_to_fetch` signal
+    assign input_to_fetch_data  = input_transaction;
+    assign input_to_fetch_valid = mptw_transaction_valid_i;
+
+    // TODO: make this depending on the whole pipeline conditions
+    assign mptw_ready_o = input_to_fetch_ready; 
+
+    //////////////////////////
+    // Fetch Stage Instance //
+    //////////////////////////
+
+    .fetch_stage # (
 
         .PIPELINE_SLAVE_DATA_WIDTH      ( fetch_stage_datawidth         ),
         .PIPELINE_MASTER_DATA_WIDTH     ( plb_lookup_stage_datawidth    )
@@ -80,8 +149,11 @@ module mptw_top #(
         .exception_cause_o      ( fetch_exception_cause             )
     );
 
-    // Fetch to PLB Lookup Pipeline Register
-    pipeline_register #(
+    ///////////////////////////////////////////
+    // Fetch to PLB Lookup Pipeline Register //
+    ///////////////////////////////////////////
+
+    .pipeline_register # ( 
 
         .DATA_WIDTH             ( plb_lookup_stage_datawidth    )
 
@@ -91,18 +163,74 @@ module mptw_top #(
         .rst_ni                 ( rst_ni                        ),
 
         `MAP_DATA_PORT          ( s_data, fetch_to_pipe         ),
-        `MAP_DATA_PORT          ( m_data, fetch_to_plb_lookup   ),
+        `MAP_DATA_PORT          ( m_data, pipe_to_plb_lookup   ),
         `SINK_SLAVE_CTRL_PORT   ( s_ctrl                        )
 
     ); 
 
-    //////////////////////
-    // PLB Lookup Stage //
-    //////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //    ___ _    ___   _             _               ___ _                    //
+    //   | _ \ |  | _ ) | |   ___  ___| |___  _ _ __  / __| |_ __ _ __ _ ___    //
+    //   |  _/ |__| _ \ | |__/ _ \/ _ \ / / || | '_ \ \__ \  _/ _` / _` / -_)   //
+    //   |_| |____|___/ |____\___/\___/_\_\\_,_| .__/ |___/\__\__,_\__, \___|   //
+    //                                         |_|                 |___/        //
+    //////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////
-    // Walking Stages //
-    ////////////////////
+    .plb_lookup_stage #(
+
+        .PIPELINE_SLAVE_DATA_WIDTH  ( plb_lookup_stage_datawidth ),
+        .PIPELINE_MASTER_DATA_WIDTH ( walking_stage_datawidth    )
+
+    ) plb_lookup_stage_u (
+        
+        .clk_i                  ( clk_i                                     ),
+        .rst_ni                 ( rst_ni                                    ),
+
+        // Pipeline Ports
+        `MAP_DATA_PORT          ( plb_lookup_slave  , pipe_to_plb_lookup    ),
+        `MAP_DATA_PORT          ( plb_lookup_master , plb_lookup_to_pipe    ),
+        `SINK_SLAVE_CTRL_PORT   ( plb_lookup_ctrl                           ),
+
+        // PLB Cache Port
+        .plb_cache_mem_req      ,
+        .plb_cache_mem_gnt      ,
+        .plb_cache_mem_valid    ,
+        .plb_cache_mem_addr     ,
+        .plb_cache_mem_rdata    ,
+        .plb_cache_mem_wdata    ,
+        .plb_cache_mem_we       ,
+        .plb_cache_mem_be       ,
+        .plb_cache_mem_error    
+
+    ); 
+
+    ///////////////////////////////////////////////////
+    //PLB Lookup to Walking Stages Pipeline Register //
+    ///////////////////////////////////////////////////
+
+    .pipeline_register # ( 
+
+        .DATA_WIDTH             ( plb_lookup_stage_datawidth    )
+
+    ) fetch_to_plb_lookup_reg_u (
+
+        .clk_i                  ( clk_i                         ),
+        .rst_ni                 ( rst_ni                        ),
+
+        `MAP_DATA_PORT          ( s_data, plb_lookup_to_pipe    ),
+        `MAP_DATA_PORT          ( m_data, pipe_to_walking       ),
+        `SINK_SLAVE_CTRL_PORT   ( s_ctrl                        )
+
+    ); 
+
+
+    //////////////////////////////////////////////////////////////////////
+    //   __      __    _ _   _             ___ _                        //
+    //   \ \    / /_ _| | |_(_)_ _  __ _  / __| |_ __ _ __ _ ___ ___    //
+    //    \ \/\/ / _` | | / / | ' \/ _` | \__ \  _/ _` / _` / -_|_-<    //
+    //     \_/\_/\__,_|_|_\_\_|_||_\__, | |___/\__\__,_\__, \___/__/    //
+    //                             |___/               |___/            //
+    //////////////////////////////////////////////////////////////////////
 
     //////////////////
     // Retire Stage //
@@ -110,8 +238,12 @@ module mptw_top #(
 
     // Optional if OoO is not supported
 
-    //////////////////
-    // Commit Stage //
-    //////////////////
+    //////////////////////////////////////////////////////////////
+    //     ___                 _ _     ___ _                    //
+    //    / __|___ _ __  _ __ (_) |_  / __| |_ __ _ __ _ ___    //
+    //   | (__/ _ \ '  \| '  \| |  _| \__ \  _/ _` / _` / -_)   //
+    //    \___\___/_|_|_|_|_|_|_|\__| |___/\__\__,_\__, \___|   //
+    //                                             |___/        //
+    //////////////////////////////////////////////////////////////
 
 endmodule : mptw_top
--- rtl/plb_lookup_stage.sv
+++ rtl/plb_lookup_stage.sv
@@ -1,5 +1,4 @@
 // Author: Stefano Mercogliano <stefano.mercogliano@unina.it>
-// Author: Valerio Di Domenico <didomenico.valerio@virgilio.it>
 
 // Description:
 //  The PLB lookup stage exposes a memory interface specifically used to access the PLB.
@@ -8,6 +7,8 @@
 //  A plb_lookup_req is made of the current Supervisor Domain ID, the Supervisor Phyisical Address
 //  And the required access permissions. If the data is available, the transaction is marked as MPT_WALKING_SKIP
 //  Protocol conversion can be applied outside of the MPT Walker to comply with a specific Cache.
+
+// TODO: Support stall and flush logic
 
 /* verilator lint_off IMPORTSTAR */
 import mpt_pkg::*;
@@ -18,9 +19,10 @@ import mpt_pkg::*;
 `include "uninasoc_mem.svh"
 
 module plb_lookup_stage #(
-    parameter unsigned PIPELINE_SLAVE_DATA_WIDTH = 32;
-    parameter unsigned PIPELINE_MASTER_DATA_WIDTH = 32;
-    localparam unsigned PLB_TRANSACTION_DATA_WIDTH = $bits(plb_lookup_req_t);
+    parameter unsigned  PIPELINE_SLAVE_DATA_WIDTH   = 32;
+    parameter unsigned  PIPELINE_MASTER_DATA_WIDTH  = 32;
+    localparam unsigned PLB_TRANSACTION_DATA_WIDTH  = 1;                        // Only interested in the hit response 
+    localparam unsigned PLB_TRANSACTION_ADDR_WIDTH  = $bits(plb_lookup_req_t); 
 ) (
     // Generic Signals
     input  logic                clk_i,
@@ -36,26 +38,47 @@ module plb_lookup_stage #(
     `DEFINE_SLAVE_CTRL_PORT(plb_lookup_control),
 
     // Cache memory interface
-    `DEFINE_MEM_MASTER_PORTS(plb_cache, PLB_TRANSACTION_DATA_WIDTH)
+    `DEFINE_MEM_MASTER_PORTS(plb_cache, PLB_TRANSACTION_DATA_WIDTH, PLB_TRANSACTION_ADDR_WIDTH)
 
 ); 
+
+    //////////////////////////////////////
+    //    ___ _                _        //
+    //   / __(_)__ _ _ _  __ _| |___    //
+    //   \__ \ / _` | ' \/ _` | (_-<    //
+    //   |___/_\__, |_||_\__,_|_/__/    //
+    //         |___/                    //
+    //////////////////////////////////////
 
     /////////////////////////
     // Signals Declaration //
     /////////////////////////
 
-    plb_lookup_req_t plb_tag_req;
-    mptw_transaction_t transaction_i, transaction_o;
-    mptw_transaction_t pre_hit_transaction, post_hit_transaction;
-    logic stage_active;
-    logic [1:0] current_state_q, next_state_d;
-    logic plb_hit_d, plb_hit_q;
-    logic plb_hit_valid_d, plb_hit_valid_q;
+    plb_lookup_req_t    plb_tag_req;
+    mptw_transaction_t  grant_stage_transaction;
+    mptw_transaction_t  valid_stage_pre_hit_transaction;
+    mptw_transaction_t  valid_stage_post_hit_transaction;
 
+    logic               plb_hit_d, plb_hit_q;
+    logic               plb_hit_valid_d, plb_hit_valid_q;
+    logic               hit_produced;
+
+    // Data bus from Grant Stage to Valid Stage
     `DECLARE_DATA_BUS(grant_to_valid, PIPELINE_SLAVE_DATA_WIDTH)
     `DECLARE_CTRL_BUS(grant_ctrl)
 
-    `DECLARE_DATA_BUS(valid_to_output, PIPELINE_SLAVE_DATA_WIDTH)
+    // Data bus from Valid Stage to the outside (next pipeline reg)
+    `DECLARE_DATA_BUS(valid_stage, PIPELINE_SLAVE_DATA_WIDTH)
+
+    ///////////////////////
+    // Bus Concatenation //
+    ///////////////////////
+
+    // Assign ctrl signals to the inner stages
+    // If the plb_lookup_stage is flushed or stalled, all the other
+    // stages must be stalled/flushed as well
+    `ASSIGN_CTRL_BUS(grant_ctrl, plb_lookup_ctrl)
+    `ASSIGN_CTRL_BUS(valid_ctrl, plb_lookup_ctrl)
 
     //////////////////////////////////////////////////////
     //    _   _                    _   _                //
@@ -65,12 +88,8 @@ module plb_lookup_stage #(
     //              |_|                      |___/      //
     //////////////////////////////////////////////////////
 
-    // First, let's unpack data into transaction_i
-    assign transaction_i = plb_lookup_slave_rdata;
-
-    // Assign ctrl signals to the inner stages
-    `ASSIGN_CTRL_BUS(grant_ctrl, plb_lookup_ctrl)
-    `ASSIGN_CTRL_BUS(valid_ctrl, plb_lookup_ctrl)
+    // Unpack data into grant_stage_transaction
+    assign grant_stage_transaction = plb_lookup_slave_rdata;
 
     //////////////////////////////////////////////////////////
     //     ___              _     ___ _                     //
@@ -85,33 +104,41 @@ module plb_lookup_stage #(
     // the next stage, leaving room for another transaction to come in
 
     // First, let's build the PLB request out of the input transaction
-    assign plb_tag_req.SDID = transaction_i.mmpt.SDID;
-    assign plb_tag_req.spa = transaction_i.spa;
-    assign plb_tag_req.access_type = transaction_i.access_type;
+    assign plb_tag_req.SDID         = grant_stage_transaction.mmpt.SDID;
+    assign plb_tag_req.spa          = grant_stage_transaction.spa;
+    assign plb_tag_req.access_type  = grant_stage_transaction.access_type;
 
-    // Second, issue a new request if the valid stage is ready 
+    // Second, issue a new plb cache request if the Valid Stage is ready.
+    // This is fundamental as when a grant is provided by the memory, in t he
+    // next clock cycles[s] an answer will come regardless of the Valid Stage status.
     always_comb begin
-        // If valid stage is ready, send out the request
+        // Deault values
+        plb_cache_mem_req   = '0;
+        plb_cache_mem_addr  = '0;
+        plb_cache_mem_we    = '0;
+        // If Valid Stage is ready, send out a read request.
+        // The request is high as long as the pipeline is working.
+        // Once the grant signal is asserted by the memory, the result
+        // Will come regardless of the MPT walker status (it will be ignored though)
         if ( grant_to_valid_ready ) begin 
-            plb_cache_mem_req = plb_lookup_slave_valid;
-            plb_cache_mem_addr = plb_tag_req;
-            plb_cache_mem_we = '0; // read transaction
+            plb_cache_mem_req   = plb_lookup_slave_valid;
+            plb_cache_mem_addr  = plb_tag_req;  
             // If the grant is received, the memory will return a value
             // in the next clock cycle[s]
             if ( plb_cache_mem_gnt ) begin
                 // We can write on the pipeline register
-                grant_to_valid_valid = 1'b1;
+                grant_to_valid_valid    = 1'b1;
                 // Next clock cycle, we can have a new request coming in
-                plb_lookup_slave_ready = 1'b1;
+                plb_lookup_slave_ready  = 1'b1;
             end else begin
-                grant_to_valid_valid = 1'b0;
-                plb_lookup_slave_ready = 1'b0;
+                grant_to_valid_valid    = 1'b0;
+                plb_lookup_slave_ready  = 1'b0;
             end
         end 
     end
 
     // The transaction value will be forwarded to the valid stage
-    assign grant_to_valid_data = transaction_i;
+    assign grant_to_valid_data = grant_stage_transaction;
 
     ///////////////////////
     // Pipeline Register //
@@ -121,12 +148,12 @@ module plb_lookup_stage #(
     // to the next stage. There is no reason to save the plb request.
 
     pipeline_register #(
-        .DATA_WIDTH             ( PIPELINE_SLAVE_DATA_WIDTH     )
+        .DATA_WIDTH             ( $bits(grant_to_valid_data)    )
     ) grant_to_valid_reg (
         .clk_i                  ( clk_i                         ),
         .rst_ni                 ( rst_ni                        ),
         `MAP_DATA_PORT          ( s_data, grant_to_valid        ),
-        `MAP_DATA_PORT          ( m_data, valid_to_output       ),
+        `MAP_DATA_PORT          ( m_data, valid_stage           ),
         `MAP_CTRL_PORT          ( s_ctrl, grant_ctrl            )
     ); 
 
@@ -142,44 +169,74 @@ module plb_lookup_stage #(
     // We assume it to be encoded into the data port for the read transaction
     // issued in the previous stage.
 
-    // Hit value is encoded in the read data transaction
-    assign plb_hit_d = ( plb_cache_mem_data ) ? 1'b1 : 1'b0 ;
-    assign plb_hit_valid_d = 1'b0; 
+    // Hit value is encoded in the read data transaction ( which is 1 bit wide )
+    // By default, the hit signal is non-valid, and the git depends on the data response.
+    assign plb_hit_d        = ( plb_cache_mem_data ) ? 1'b1 : 1'b0 ;
+    assign plb_hit_valid_d  = plb_cache_mem_valid; 
 
-    // The valid stage is not free to go once the hit value is retrieved, as the next stage
-    // might not be available. However, the cache does not need to stall, therefore we
-    // need to buffer the the hit bit.
+    // Once the `plb_cache_mem_valid` signal is asserted, the hit value is correctly retrieved.
+    // Nevertheless, The valid stage is not free to go once the hit value is retrieved, as the next stage
+    // might not be available. However, the cache does not need to stall, therefore we must buffer
+    // the hit bit, and use it when the next stage is ready to process it.
+    assign hit_produced = plb_hit_valid_d || plb_hit_valid_q;
     always_ff @(posedge clk_i) begin
-        if (~rst_ni || plb_lookup_control_flush) begin
+        if (~rst_ni) begin
             plb_hit_q <= '0;
         end else begin
-            if ( plb_cache_mem_valid ) begin 
-                plb_hit_q <= plb_hit_d;
-                plb_hit_valid_d <= 1'b1
+            // This FFs follow a producer-consumer logic. The producer is the memory ( `plb_cache_mem_valid` ).
+            // The consumer is the Next stage ( `plb_lookup_master_ready` ).
+            if ( plb_cache_mem_valid && ~plb_lookup_master_ready ) begin 
+                // Data is produced, but the consumer is not ready.
+                // Therefore, buffer the data
+                plb_hit_q       <= plb_hit_d;
+                plb_hit_valid_q <= plb_hit_valid_d;
+            else if ( plb_hit_valid_q ) begin 
+                // If the produced response was buffered, we need to wait for the consumer
+                plb_hit_q       <= ( plb_lookup_master_ready ) ? '0 : plb_hit_q ;
+                plb_hit_valid_q <= ( plb_lookup_master_ready ) ? '0 : plb_hit_valid_q ;
+            end else begin
+                // In all other cases, just keep the FFs low
+                plb_hit_q       <= '0;
+                plb_hit_valid_q <= '0;
             end
         end
     end
 
+    // This stage is ready to accept a transaction only if the data is produced, and
+    // the next stage (i.e. the consumer) is ready (i.e. will get the data in the next clock cycle)
+    assign valid_stage_ready = (hit_produced) && plb_lookup_master_ready;
+
     // Once the plb hit value is valid, and the next stage is ready, we can update the transaction value
-    assign pre_hit_transaction = valid_to_output_data;
+    // Depending on the hit; if the transaction was found in the PLB, then the walking is avoided,
+    // otherwise it must be performed.
+    assign valid_stage_pre_hit_transaction = valid_stage_data;
 
     always_comb begin
 
-        post_hit_transaction.mmpt = pre_hit_transaction.mmpt;
-        post_hit_transaction.spa = pre_hit_transaction.spa;
-        post_hit_transaction.access_type = pre_hit_transaction.access_type;
+        valid_stage_post_hit_transaction.mmpt = valid_stage_pre_hit_transaction.mmpt;
+        valid_stage_post_hit_transaction.spa = valid_stage_pre_hit_transaction.spa;
+        valid_stage_post_hit_transaction.access_type = valid_stage_pre_hit_transaction.access_type;
 
-        if ( (plb_hit_valid_d || plb_hit_valid_q) && plb_lookup_master_ready ) begin 
-            post_hit_transaction.walking = ( plb_cache_mem_data ) ? MPT_WALKING_SKIP : MPT_WALKING_DO ;
+        if ( valid_stage_ready ) begin 
+            valid_stage_post_hit_transaction.walking = ( ( plb_hit_q || plb_hit_d ) ) ? MPT_WALKING_SKIP : MPT_WALKING_DO ;
         end 
     end
 
-    // down here connect the last bits
+    //////////////////////////////////////////////////
+    //    ___                   _   _               //
+    //   | _ \___ _ __  __ _ __| |_(_)_ _  __ _     //
+    //   |   / -_) '_ \/ _` / _| / / | ' \/ _` |    //
+    //   |_|_\___| .__/\__,_\__|_\_\_|_||_\__, |    //
+    //           |_|                      |___/     //
+    //////////////////////////////////////////////////
 
-    //////////////////////////////////
-    // Pipeline Master Port Packing //
-    //////////////////////////////////
+    // Finally, we can repack the signals to the next pipeline register/stage.
+    // The output data is the `valid_stage_post_hit_transaction`
+    assign plb_lookup_master_data = valid_stage_post_hit_transaction;
 
-    `ASSIGN_DATA_BUS(fetch_master, slave_master)
+    // The data is valid then hit has been produced
+    assign plb_lookup_master_valid = hit_produced;
+
     
 endmodule : plb_lookup_stage
+
